f75f890f5557a54359206a1ee3cac10be8d81424
ThArN <ThArN@Panitan>
2014-01-21 14:16:57 +0700

    @New Menu

---
+++ public_html/register-ny.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>NiyomPanich Co.,Ltd.</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="navbar navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container">
+                    <a class="brand" href="#">eRecruit</a>
+                    <ul class="nav">
+                        <li class="active"><a href="#"><img src="img/nylogo-bw.gif" width="25">&nbsp;นิยมพานิช</a></li>
+                        <li><a href="#"><img src="img/LogoNorthwave-bw.gif" width="50">&nbsp;นอร์ทเวฟ</a></li>
+                        <li><a href="#"><img src="img/ToyotaChiangmai-bw.gif" width="25">&nbsp;โตโยต้า เชียงใหม่</a></li>
+                        <li><a href="#"><img src="img/logowinwave-bw.gif" width="30">&nbsp;วินเวฟ</a></li>
+                        <li><a href="#"><img src="img/LogoMatrix-bw.gif" width="40">&nbsp;แมทริกซ์</a></li>
+                        <li><a href="#"><img src="img/logomsd-bw.gif" width="25">&nbsp;มิตรสินด่วน</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <br><br><br>
+        <?php echo '1234'; ?>
+        
+                
+        <script src="js/jquery-1.10.2.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
