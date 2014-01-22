e7aa74da92e5aaa97e7ee87b9beb246d795479e1
ThArN <ThArN@Panitan>
2014-01-22 16:25:19 +0700

    @Complete Menu+New look Login

--- public_html/register-ny.php
+++ public_html/register-ny.php
@@ -15,21 +15,103 @@ and open the template in the editor.
         <div class="navbar navbar-fixed-top">
             <div class="navbar-inner">
                 <div class="container">
-                    <a class="brand" href="#">eRecruit</a>
+                    <label class="brand" >eRECRUITMENT</label>
                     <ul class="nav">
                         <li class="active"><a href="#"><img src="img/nylogo-bw.gif" width="25">&nbsp;นิยมพานิช</a></li>
-                        <li><a href="#"><img src="img/LogoNorthwave-bw.gif" width="50">&nbsp;นอร์ทเวฟ</a></li>
                         <li><a href="#"><img src="img/ToyotaChiangmai-bw.gif" width="25">&nbsp;โตโยต้า เชียงใหม่</a></li>
-                        <li><a href="#"><img src="img/logowinwave-bw.gif" width="30">&nbsp;วินเวฟ</a></li>
-                        <li><a href="#"><img src="img/LogoMatrix-bw.gif" width="40">&nbsp;แมทริกซ์</a></li>
-                        <li><a href="#"><img src="img/logomsd-bw.gif" width="25">&nbsp;มิตรสินด่วน</a></li>
+                        <li><a href="#"><img src="img/logowinwave-bw.gif" width="25">&nbsp;วินเวฟ</a></li>
+                        <li><a href="#"><img src="img/LogoMatrix-bw.gif" width="35">&nbsp;แมทริกซ์</a></li>
+                        <li><a href="#"><img src="img/LogoNorthwave-bw.gif" width="35">&nbsp;นอร์ทเวฟ</a></li>
+                        <li><a href="#"><img src="img/logomsd-bw.gif" width="35">&nbsp;มิตรสินด่วน</a></li>
                     </ul>
                 </div>
             </div>
         </div>
         <br><br><br>
-        <?php echo '1234'; ?>
-        
+        <div class="container" id="border">
+            <div>
+                <img src="img/nylogo.gif" width="80">
+                &nbsp;&nbsp;
+                <img src="img/nytxt.gif" width="300">
+            </div>
+            <br>
+            <div class="form-inline">
+                วันที่สมัคร&nbsp;<input class="span2" type="datetime" name="regdate">
+                ตำแหน่ง&nbsp;1.&nbsp;<input type="text" name="regdate">
+                2.&nbsp;<input type="text" name="regdate">
+            </div>
+            <br>
+            <div class="well">
+                <form id="signup" class="form-horizontal" method="post" action="success.php">
+                    <legend>ข้อมูลส่วนตัว</legend>
+                </form>
+            </div>
+            <div class="well">
+                <form id="signup" class="form-horizontal" method="post" action="success.php">
+                    <legend>เอกสารประจำตัว</legend>
+                </form>
+            </div>
+            <div class="well">
+                <form id="signup" class="form-horizontal" method="post" action="success.php">
+                    <legend>ที่อยู่</legend>
+                </form>
+            </div>
+            <div class="well">
+                <form id="signup" class="form-horizontal" method="post" action="success.php">
+                    <legend>ประวัติการศึกษา</legend>
+                </form>
+            </div>
+            <div class="well">
+                <form id="signup" class="form-horizontal" method="post" action="success.php">
+                    <legend>ประวัติการทำงาน</legend>
+                </form>
+            </div>
+            <div class="well">
+                <form id="signup" class="form-horizontal" method="post" action="success.php">
+                    <legend>ประวัติการฝึกงาน</legend>
+                </form>
+            </div>
+            <div class="well">
+                <form id="signup" class="form-horizontal" method="post" action="success.php">
+                    <legend>ความสามารถ</legend>
+                </form>
+            </div>
+            <div class="well">
+                <form id="signup" class="form-horizontal" method="post" action="success.php">
+                    <legend>การฝึกอบรม</legend>
+                </form>
+            </div>
+            <div class="well">
+                <form id="signup" class="form-horizontal" method="post" action="success.php">
+                    <legend>ข้อมูลคู่สมรส</legend>
+                </form>
+            </div>
+            <div class="well">
+                <form id="signup" class="form-horizontal" method="post" action="success.php">
+                    <legend>ประวัติครอบครัว</legend>
+                </form>
+            </div>
+            <div class="well">
+                <form id="signup" class="form-horizontal" method="post" action="success.php">
+                    <legend>บุคคลอ้างอิง หรือผู้ที่สามารถติดต่อได้</legend>
+                </form>
+            </div>
+            <div class="well">
+                <form id="signup" class="form-horizontal" method="post" action="success.php">
+                    <legend>สิ่งค้ำประกัน</legend>
+                </form>
+            </div>
+            <div class="well">
+                <form id="signup" class="form-horizontal" method="post" action="success.php">
+                    <legend>ความสนใจเกี่ยวกับงานของบริษัท</legend>
+                </form>
+            </div>
+            <div class="well">
+                <form id="signup" class="form-horizontal" method="post" action="success.php">
+                    <legend>ท่านทราบตำแหน่งงานว่างได้อย่างไร</legend>
+                </form>
+            </div>
+        </div>
                 
         <script src="js/jquery-1.10.2.js"></script>
         <script src="js/bootstrap.min.js"></script>
